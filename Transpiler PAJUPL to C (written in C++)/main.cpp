001051552dce54a131310d581a99af8e7ceaac14
SuPeRTRoNeRD <30605599+SuPeRTRoNeRD@users.noreply.github.com>
2020-12-08 10:26:26 +0100

    Stop using namespace std and use cout and cin instead

--- Transpiler PAJUPL to C (written in C++)/main.cpp
+++ Transpiler PAJUPL to C (written in C++)/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 
-using namespace std;
+using std::cout;
+using std::cin;
 
 int main(int argc, char const *argv[]) {
-    cout << "This is the end of the transpiler" << endl;
+    cout << "This is the end of the transpiler.\n";
     return 0;
 }
